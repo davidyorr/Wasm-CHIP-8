377c649b5e2023450f946dff77dbeff4fdd0f956
David Orr <davidyorr@gmail.com>
2025-05-26 14:50:53 -0400

    Add a basic "hello world" for go wasm

---
+++ go.mod
@@ -0,0 +1,3 @@
+module wasm-chip-8
+
+go 1.24.3
---
+++ server/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatalln(err)
+}
