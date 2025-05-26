25b18c80314c63dc9eef4598f72bd901e140a05f
David Orr <davidyorr@gmail.com>
2025-05-26 18:11:29 -0400

    Add a canvas element

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 )
 
 func main() {
 	fmt.Println("Hello Wasm")
+
+	fmt.Println("go func()")
+	go func() {
+		fmt.Println("calling draw()")
+		draw()
+	}()
+
+	fmt.Println("setup complete, blocking main goroutine")
+	<-make(chan struct{})
+}
+
+func draw() {
+	document := js.Global().Get("document")
+	canvas := document.Call("getElementById", "canvas")
+	ctx := canvas.Call("getContext", "2d")
+	width := canvas.Get("width").Int()
+	height := canvas.Get("height").Int()
+	ctx.Set("fillStyle", "#EEE")
+	ctx.Call("fillRect", 0, 0, width, height)
 }
