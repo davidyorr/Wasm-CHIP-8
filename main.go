1f7ae07b04fa6db0b97dac7bea6f0e60aa70174a
David Orr <davidyorr@gmail.com>
2025-05-27 02:20:06 -0400

    Add a button for selecting a ROM to load into memory

--- main.go
+++ main.go
@@ -5,17 +5,57 @@ import (
 	"syscall/js"
 )
 
+const (
+	memorySize = 4096
+	// (512) start of most CHIP-8 programs
+	programOffset = 0x200
+)
+
+// RAM
+var memory [memorySize]uint8
+// program counter
+var PC uint16
+// index register
+var I uint16
+// stack
+var stack []uint16
+
 func main() {
 	fmt.Println("Hello Wasm")
 
-	fmt.Println("go func()")
+	setUpRomLoader()
+
 	go func() {
-		fmt.Println("calling draw()")
 		draw()
 	}()
 
 	fmt.Println("setup complete, blocking main goroutine")
 	<-make(chan struct{})
+}
+
+func setUpRomLoader() {
+	document := js.Global().Get("document")
+	fileInput := document.Call("getElementById", "rom-input")
+	fileInput.Set("oninput", js.FuncOf(func(this js.Value, args []js.Value) any {
+		fileInput.Get("files").Call("item", 0).Call("arrayBuffer").Call("then", js.FuncOf(func(this js.Value, args []js.Value) any {
+			jsRomData := js.Global().Get("Uint8Array").New(args[0])
+			goDstSlice := make([]byte, jsRomData.Get("length").Int())
+			js.CopyBytesToGo(goDstSlice, jsRomData)
+			fmt.Println("data", jsRomData)
+
+			// clear any memory from previous ROM
+			clear(memory[programOffset:])
+
+			// copy the ROM data into the memory variable
+			numberOfBytesCopied := copy(memory[programOffset:], goDstSlice)
+
+			fmt.Printf("Loaded %d bytes of ROM into memory array\n", numberOfBytesCopied)
+
+			return nil
+		}))
+
+		return nil
+	}))
 }
 
 func draw() {
