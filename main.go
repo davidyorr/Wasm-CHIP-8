15d6e6c44837cabfed56113bdde87458ad42c4cc
David Orr <davidyorr@gmail.com>
2025-05-31 20:36:54 -0400

    Implement more instructions and add the font

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"syscall/js"
 	"time"
 )
@@ -10,11 +11,12 @@ const (
 	memorySize = 4096
 	// (512) start of most CHIP-8 programs
 	programOffset = 0x200
+	fontOffset    = 0x050
 	displayWidth  = 64
 	displayHeight = 32
 	// the display is 64 x 32, so multiply each pixel by the drawScale
 	drawScale = 10
-	// 60hz - 1 million microseconds in a second
+	// 60Hz - 1 million microseconds in a second
 	thresholdMicroseconds = (1.0 / 60.0) * 1000000.0
 )
 
@@ -24,14 +26,18 @@ var memory [memorySize]uint8
 // program counter
 var PC uint16
 
-// index register
+// general purpose registers, referred to as VX, where X is a hex digit
+var V [16]uint8
+
+// timers
+var delayTimer uint8
+var soundTimer uint8
+
+// index register, used to store memory addresses
 var I uint16
 
 // stack
 var stack []uint16
-
-// general purpose registers
-var register [16]uint8
 
 // true for "on", false for "off"
 var outputBuffer [displayHeight][displayWidth]bool
@@ -40,8 +46,12 @@ var outputBuffer [displayHeight][displayWidth]bool
 var timeAccumulator float64
 var lastFrameTime time.Time
 
+// to keep track of which keys are currently pressed, true for pressed
+var keypadStates [16]bool
+
 // for debugging
 var romLength int
+var romIsRunning bool = false
 
 func main() {
 	fmt.Println("Hello Wasm")
@@ -59,82 +69,369 @@ func processEmulatorStep() {
 	timeAccumulator += float64(delta.Microseconds())
 
 	for timeAccumulator >= thresholdMicroseconds {
-		executeInstruction()
+		// target 660 IPS
+		// 60Hz is timers target, so execute 11 instructions per tick
+		for range 11 {
+			executeInstruction()
+		}
+		// decrement timers once per 60Hz
+		if delayTimer > 0 {
+			delayTimer--
+		}
+		if soundTimer > 0 {
+			soundTimer--
+		}
 		timeAccumulator -= thresholdMicroseconds
 	}
+	lastFrameTime = time.Now()
 }
 
 func executeInstruction() {
+	if !romIsRunning {
+		return
+	}
+
 	// instructions are 2 bytes
 	instruction := (uint16(memory[PC]) << 8) | uint16(memory[PC+1])
-	fmt.Printf("PC=[%d], handling instruction [%04X] [%16b]\n", PC, instruction, instruction)
+	// fmt.Printf("PC=[%d], handling instruction [%04X] [%16b]\n", PC, instruction, instruction)
 
 	// look at the first nibble
 	switch (instruction & 0xF000) >> 12 {
 	case 0x0:
 		{
-			if uint8(instruction&0x00FF) == 0xE0 {
-				fmt.Println("clear screen")
-				clearScreen()
+			// 0NNN is only used on certain old computers
+
+			switch uint8(instruction & 0x00FF) {
+			case 0xE0:
+				{
+					// 00E0 : clear screen
+					clearScreen()
+
+				}
+			case 0xEE:
+				{
+					// 00EE : return from subroutine
+					PC, stack = stack[len(stack)-1], stack[:len(stack)-1]
+				}
+			default:
+				{
+					stopForUnhandledInstruction(instruction)
+					return
+				}
 			}
 		}
 	case 0x1:
 		{
-			// 1NNN : Jump
+			// 1NNN : jump
 			// set PC to NNN
 			nnn := instruction & 0xFFF
 			PC = nnn
 			// we don't want to increment PC again, so return here
 			return
 		}
+	case 0x2:
+		{
+			// 2NNN : subroutine
+			// call subroutine at NNN
+			nnn := instruction & 0xFFF
+			stack = append(stack, PC)
+			PC = nnn
+			return
+		}
+	case 0x3:
+		{
+			// 3XNN : skip conditional
+			// skip the next instruction if VX equals NN
+			vx := V[(instruction&0x0F00)>>8]
+			nn := uint8(instruction & 0x00FF)
+			if vx == nn {
+				PC += 2
+			}
+		}
+	case 0x4:
+		{
+			// 4XNN : skip conditional
+			// skip the next instruction if VX does equals NN
+			vx := V[(instruction&0x0F00)>>8]
+			nn := uint8(instruction & 0x00FF)
+			if vx != nn {
+				PC += 2
+			}
+		}
+	case 0x5:
+		{
+			// 5XY0 : skip conditional
+			// skip the next instruction if VX equals VY
+			vx := V[(instruction&0x0F00)>>8]
+			vy := V[(instruction&0x00F0)>>4]
+			if vx == vy {
+				PC += 2
+			}
+		}
 	case 0x6:
 		{
+			// 6XNN : set
 			// set the register VX to the value NN
-			registerIndex := (instruction & 0x0F00) >> 8
-			value := uint8(instruction & 0x00FF)
-			fmt.Printf("setting register %d to [%02X]\n", registerIndex, value)
-			register[registerIndex] = value
+			x := (instruction & 0x0F00) >> 8
+			nn := uint8(instruction & 0x00FF)
+			V[x] = nn
 		}
 	case 0x7:
 		{
-			// 7XNN
+			// 7XNN : add
 			// add the value of NN to VX
-			vx := (instruction & 0x0F00) >> 8
+			x := (instruction & 0x0F00) >> 8
 			nn := uint8(instruction & 0x00FF)
-			fmt.Printf("7XNN -> add nn=[%d] to vx=[%02X]\n", nn, vx)
-			register[vx] += nn
+			V[x] += nn
+		}
+	case 0x8:
+		{
+			switch (instruction & 0x000F) >> 0 {
+			case 0x0:
+				{
+					// 8XY0 : set
+					// set VX to the value of VY
+					x := (instruction & 0x0F00) >> 8
+					y := (instruction & 0x00F0) >> 4
+					V[x] = V[y]
+				}
+			case 0x1:
+				{
+					// 8XY1 : bitwise OR
+					// set VX to the OR of VX and VY
+					x := (instruction & 0x0F00) >> 8
+					y := (instruction & 0x00F0) >> 4
+					V[x] = V[x] | V[y]
+				}
+			case 0x2:
+				{
+					// 8XY2 : bitwise AND
+					// set VX to the AND of VX and VY
+					x := (instruction & 0x0F00) >> 8
+					y := (instruction & 0x00F0) >> 4
+					V[x] = V[x] & V[y]
+				}
+			case 0x3:
+				{
+					// 8XY3 : bitwise XOR
+					// set VX to the XOR of VX and VY
+					x := (instruction & 0x0F00) >> 8
+					y := (instruction & 0x00F0) >> 4
+					V[x] = V[x] ^ V[y]
+				}
+			case 0x4:
+				{
+					// 8XY4 : add
+					// set VX to the sum of VX and VY
+					// if the result overflowed, set VF to 1
+					x := (instruction & 0x0F00) >> 8
+					y := (instruction & 0x00F0) >> 4
+					V[x] = V[x] + V[y]
+					if V[x] < V[y] {
+						V[0xF] = 1
+					} else {
+						V[0xF] = 0
+					}
+				}
+			case 0x5:
+				{
+					// 8XY5 : subtract
+					// set VX to the difference of VX and VY
+					x := (instruction & 0x0F00) >> 8
+					y := (instruction & 0x00F0) >> 4
+					if V[x] > V[y] {
+						V[0xF] = 1
+					} else {
+						V[0xF] = 0
+					}
+					V[x] = V[x] - V[y]
+				}
+			case 0x6:
+				{
+					// 8XY6 : shift
+					// shift the value of VX one bit to the right
+					x := (instruction & 0x0F00) >> 8
+					shiftedOutBit := V[x] & 1
+					V[0xF] = shiftedOutBit
+					V[x] = V[x] >> 1
+				}
+			case 0x7:
+				{
+					// 8XY7 : subtract
+					// set VX to the difference of VY and VX
+					x := (instruction & 0x0F00) >> 8
+					y := (instruction & 0x00F0) >> 4
+					if V[y] > V[x] {
+						V[0xF] = 1
+					} else {
+						V[0xF] = 0
+					}
+					V[x] = V[y] - V[x]
+				}
+			case 0xE:
+				{
+					// 8XYE : shift
+					// shift the value of VX one bit to the left
+					x := (instruction & 0x0F00) >> 8
+					shiftedOutBit := V[x] >> 7
+					V[0xF] = shiftedOutBit
+					V[x] = V[x] << 1
+				}
+			default:
+				{
+					stopForUnhandledInstruction(instruction)
+					return
+				}
+			}
+		}
+	case 0x9:
+		{
+			// 9XY0 : skip conditional
+			// skip the next instruction if VX does equals VY
+			x := (instruction & 0x0F00) >> 8
+			y := (instruction & 0x00F0) >> 4
+			if V[x] != V[y] {
+				PC += 2
+			}
 		}
 	case 0xA:
 		{
-			fmt.Println("set I to nnn")
+			// ANNN : set index
 			// set I to NNN
 			nnn := instruction & 0xFFF
-			fmt.Printf("set I to [%012b]\n", nnn)
-			I = instruction & 0xFFF
+			I = nnn
+		}
+	case 0xB:
+		{
+			// BNNN : jump with offset
+			nnn := instruction & 0xFFF
+			PC = nnn + uint16(V[0])
+			return
+		}
+	case 0xC:
+		{
+			// CXNN : random
+			// set VX to the result of a random number bitwise ANDed with NN
+			x := (instruction & 0x0F00) >> 8
+			randomNumber := uint8(rand.UintN(256))
+			nn := uint8(instruction & 0x00FF)
+			V[x] = randomNumber & nn
 		}
 	case 0xD:
 		{
-			// DXYN draw N pixels tall sprite from memory location I, at X Y coord from register
+			// DXYN : draw
+			// draw N pixels tall sprite from memory location I, at X Y coord from register
 			// the starting position of the drawing should wrap, but not the actual drawing itself
-			vx := (instruction & 0x0F00) >> 8
-			vy := (instruction & 0x00F0) >> 4
+			x := (instruction & 0x0F00) >> 8
+			y := (instruction & 0x00F0) >> 4
 			n := (instruction & 0x000F) >> 0
-			fmt.Printf("drawing DXYN -> [D%X%X%X]\n", vx, vy, n)
-			drawSprite(vx, vy, n)
+			drawSprite(x, y, n)
+		}
+	case 0xE:
+		{
+			switch uint8(instruction & 0x00FF) {
+			case 0x9E:
+				{
+					// EX9E : skip if key
+					// skip one instruction if the VX key is currently pressed
+					x := (instruction & 0x0F00) >> 8
+					if keypadStates[V[x]] {
+						PC += 2
+					}
+				}
+			case 0xA1:
+				{
+					// EXA1 : skip if key
+					// skip one instruction if the VX key is not currently pressed
+					x := (instruction & 0x0F00) >> 8
+					if !keypadStates[V[x]] {
+						PC += 2
+					}
+				}
+			default:
+				{
+					stopForUnhandledInstruction(instruction)
+					return
+				}
+			}
+		}
+	case 0xF:
+		{
+			switch uint8(instruction & 0x00FF) {
+			case 0x0A:
+				{
+					// FX0A : get key
+					// stop executing until a key is pressed,
+					// then store the value of that key in VX
+					x := (instruction & 0x0F00) >> 8
+					var keyPtr *uint8 = nil
+					for i, pressed := range keypadStates {
+						if pressed {
+							hexValue := uint8(i)
+							keyPtr = &hexValue
+							break
+						}
+					}
+					if keyPtr == nil {
+						return
+					}
+					V[x] = *keyPtr
+				}
+			case 0x07:
+				{
+					// FX07 : timer
+					// set VX to the value of the delay timer
+					x := (instruction & 0x0F00) >> 8
+					V[x] = delayTimer
+				}
+			case 0x15:
+				{
+					// FX15 : timer
+					// set the delay timer to the value in VX
+					x := (instruction & 0x0F00) >> 8
+					delayTimer = V[x]
+				}
+			case 0x18:
+				{
+					// FX18 : timer
+					// set the sound timer to the value in VX
+					x := (instruction & 0x0F00) >> 8
+					soundTimer = V[x]
+				}
+			case 0x1E:
+				{
+					// FX1E : add to index
+					// add the value of VX to index register I
+					x := (instruction & 0x0F00) >> 8
+					I += uint16(V[x])
+				}
+			case 0x29:
+				{
+					// FX29 : font character
+					// set I to the address of the hex character in VX (take the last nibble)
+					x := (instruction & 0x0F00) >> 8
+					I = uint16((V[x] & 0x000F) >> 4)
+				}
+			default:
+				{
+					stopForUnhandledInstruction(instruction)
+					return
+				}
+			}
 		}
 	default:
 		{
-			fmt.Printf("unhandled instruction: [%04X]\n", instruction)
+			stopForUnhandledInstruction(instruction)
+			return
 		}
 	}
 	PC += 2
 }
 
 func drawSprite(xReg uint16, yReg uint16, height uint16) {
-	startingX := register[xReg] & 63
+	startingX := V[xReg] & 63
 	x := startingX
-	y := register[yReg] & 63
-	fmt.Printf("attempting to draw sprite from %d\n", memory[I])
+	y := V[yReg] & 63
 
 	pixelWasTurnedOff := false
 	for line := I; line < I+height; line++ {
@@ -153,9 +450,9 @@ func drawSprite(xReg uint16, yReg uint16, height uint16) {
 		y++
 	}
 	if pixelWasTurnedOff {
-		register[0xF] = 1
+		V[0xF] = 1
 	} else {
-		register[0xF] = 0
+		V[0xF] = 0
 	}
 
 	goImageData := make([]byte, displayWidth*displayHeight*4)
@@ -197,6 +494,21 @@ func drawSprite(xReg uint16, yReg uint16, height uint16) {
 	ctx.Call("drawImage", jsOffscreenCanvas, 0, 0, displayWidth*drawScale, displayHeight*drawScale)
 }
 
+//go:wasmexport setKeypadState
+func setKeypadState(key uint32, state bool) {
+	if key > 0xF {
+		fmt.Println("unsupported key input:", key)
+		return
+	}
+	keypadStates[key] = state
+}
+
+func stopForUnhandledInstruction(instruction uint16) {
+	fmt.Printf("unhandled instruction: [%04X]\n", instruction)
+	js.Global().Get("onUnhandledInstruction").Invoke()
+	romIsRunning = false
+}
+
 func setUpRomLoader() {
 	document := js.Global().Get("document")
 	fileInput := document.Call("getElementById", "rom-input")
@@ -219,6 +531,8 @@ func setUpRomLoader() {
 			PC = programOffset
 			timeAccumulator = 0.0
 			lastFrameTime = time.Now()
+			romIsRunning = true
+			loadFont()
 
 			js.Global().Get("onRomLoaded").Invoke()
 
@@ -237,6 +551,34 @@ func clearScreen() {
 	height := canvas.Get("height").Int()
 	ctx.Set("fillStyle", "#222")
 	ctx.Call("fillRect", 0, 0, width, height)
+}
+
+func loadFont() {
+	font := [16 * 5]uint8{
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+	}
+	iMemory := fontOffset
+	iFont := 0
+	for iMemory < fontOffset+len(font) {
+		memory[iMemory] = font[iFont]
+		iMemory++
+		iFont++
+	}
 }
 
 //go:wasmexport debugRom
